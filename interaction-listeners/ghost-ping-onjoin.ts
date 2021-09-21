fdad0f209a349864d8ce3f4fd410c3e4bd228d3d
FrankMerin <>
2021-09-21 01:12:00 +0000

    adding listener for member join

---
+++ interaction-listeners/ghost-ping-onjoin.ts
@@ -0,0 +1,21 @@
+import { Message, GuildMember, TextChannel, Formatters } from "discord.js";
+import { ClientListeners } from "./index";
+import {
+  ROLE_ASSIGNMENT_CHANNEL_ID,
+  BARUCH_GUILD_ID
+} from "../assets/constants";
+import awaitTimeout from "../utils/awaitTimeout";
+
+export default class GhostPingOnjoin extends ClientListeners {
+  constructor() {
+    super("guildMemberAdd");
+  }
+  async handler(member: GuildMember): Promise<void> {
+  const baruchGuild = await member.client.guilds.fetch(BARUCH_GUILD_ID);
+  const channel = (await baruchGuild.channels.fetch(ROLE_ASSIGNMENT_CHANNEL_ID)) as TextChannel;
+
+  const msg = await channel.send(`${Formatters.userMention(member.id)}`);
+  await awaitTimeout(500);
+  msg.delete();
+  }
+}
